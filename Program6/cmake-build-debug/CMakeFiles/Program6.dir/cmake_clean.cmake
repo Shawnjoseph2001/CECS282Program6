c95719da4b08ad38cc1c81e0a38c7715d932324d
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2020-11-25 18:53:49 -0800

    /

---
+++ Program6/cmake-build-debug/CMakeFiles/Program6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program6.dir/main.cpp.o"
+  "Program6"
+  "Program6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
